fadd5e892961b1c60f6d592bec0a4ab684b9676d
jeherr <johnherr@gmail.com>
2019-03-28 14:53:27 -0400

    updated modes, updated model

--- evaluate.py
+++ evaluate.py
@@ -6,7 +6,7 @@ def main():
 	b = sys.argv[2]
 	c = sys.argv[3]
 	d = sys.argv[4]
-	model = network_model.NNModel(name="ElpasEM_Thu_Mar_21_13.16.26_2019")
+	model = network_model.NNModel(name="ElpasEM_Wed_Mar_27_16.36.09_2019")
 	model.evaluate([a, b, c, d])
 
 if __name__ == "__main__":
